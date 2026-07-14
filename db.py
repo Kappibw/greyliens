ff9b4b915d64e9912233464844174372faae7afc
Evas-diary <Evadengende@gmail.com>
2026-07-14 12:06:20 +0200

    fix: update README and improve database configuration details; enhance app.py and forum.html for better user experience

--- db.py
+++ db.py
@@ -14,9 +14,6 @@ DATABASE_URL = os.getenv("DATABASE_URL")
 if not DATABASE_URL:
     raise ValueError("DATABASE_URL is missing from .env")
 
-print("LOADED DATABASE:", DATABASE_URL)
-
-
 def get_conn():
     return psycopg2.connect(DATABASE_URL.strip())
 
